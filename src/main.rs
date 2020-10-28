043c9c431fac3d00b42a1716dfeccc9c870becb0
Zequez <zequez@gmail.com>
2020-10-28 15:10:58 -0300

    Allow zooming and moving center

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,16 @@ const BLOCKS: u32 = 110; // Horizontal blocks of text
 const LINES: u32 = ((BLOCKS * PX_W) as f64 / GOLDEN_RATIO / PX_H as f64) as u32; // Vertical lines
 const WIDTH: u32 = PX_W * BLOCKS; // Screen width
 const HEIGHT: u32 = PX_H * LINES; // Screen height
-const VIEWPORT_W: f64 = 4.0; // Virtual plot width
-const VIEWPORT_X: f64 = -2.0; // Virtual plot height
-const RATIO: f64 = VIEWPORT_W as f64 / WIDTH as f64;
-const VIEWPORT_H: f64 = HEIGHT as f64 * RATIO;
-const VIEWPORT_Y: f64 = -VIEWPORT_H / 2.0;
+
+const VIEWPORT: (f64, f64) = (-0.1, 0.95); // Move the center of the plot
+const VIEWPORT_WIDTH: f64 = 0.1; // Make this smaller for zoom
+const RATIO: f64 = VIEWPORT_WIDTH / WIDTH as f64;
+const VIEWPORT_HEIGHT: f64 = HEIGHT as f64 * RATIO;
+const FROM_X: f64 = -(VIEWPORT_WIDTH / 2.0) + VIEWPORT.0;
+const FROM_Y: f64 = -(VIEWPORT_HEIGHT / 2.0) + VIEWPORT.1;
 
 // Z ^ 2 + C
-fn mandelbrot(x: f64, y: f64) -> u16 {
+fn mandelbrot(x: f64, y: f64) -> u32 {
     let mut it = 0;
     let mut a: f64 = 0.0;
     let mut b: f64 = 0.0;
@@ -32,7 +34,7 @@ fn mandelbrot(x: f64, y: f64) -> u16 {
         b2 = b * b;
         it = it + 1;
     }
-    ((it as f64 / ITERATIONS as f64) * 9.0).round() as u16
+    it
 }
 
 const CHARACTERS: [char; 10] = ['@', '%', '#', '*', '+', '=', '-', ':', '.', ' '];
@@ -42,11 +44,11 @@ fn main() {
     for line in 0..LINES {
         let mut buf = String::from("");
         let pos_y: f64 = (line * PX_H) as f64 + PX_H as f64 / 2.0;
-        let y: f64 = pos_y * RATIO + VIEWPORT_Y;
+        let y: f64 = pos_y * RATIO + FROM_Y;
 
         for block in 0..BLOCKS {
             let pos_x: f64 = (block * PX_W) as f64 + PX_W as f64 / 2.0;
-            let x: f64 = pos_x * RATIO + VIEWPORT_X;
+            let x: f64 = pos_x * RATIO + FROM_X;
 
             // let i: u32 = ((x * x + y * y).sqrt() / RADIUS_OF_NO_RETURN * 9.0) as u32;
             // if i <= 9 {
@@ -55,9 +57,10 @@ fn main() {
             //     buf.push(CHARACTERS[9]);
             // }
 
-            let i = mandelbrot(x, y);
-            if i <= 9 {
-                buf.push(CHARACTERS[9 - i as usize]);
+            let iterations = mandelbrot(x, y);
+            let depth = ((iterations as f64 / ITERATIONS as f64) * 9.0).round() as usize;
+            if depth <= 9 {
+                buf.push(CHARACTERS[9 - depth]);
             } else {
                 panic!("Function should return value from 0 to 9");
             }
